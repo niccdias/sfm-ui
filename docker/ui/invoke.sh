4d98f25c44f6758f04ae0a11202f4ad39d1ca413
Justin Littman <justinlittman@gwu.edu>
2017-02-09 16:05:00 -0500

    refs #361. HTTPS setup.

--- docker/ui/invoke.sh
+++ docker/ui/invoke.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ -f /opt/sfm-setup/server.crt ]; then
+    echo "Copying SSL certificate"
+    cp /opt/sfm-setup/server.crt /etc/ssl/certs/server.crt
+    cp /opt/sfm-setup/server.key /etc/ssl/private/server.key
+    chown root:ssl-cert /etc/ssl/private/server.key
+    chmod 0640 /etc/ssl/private/server.key
+    echo "Adding sfm user to ssl-cert group"
+    usermod -a -G ssl-cert sfm
+fi
+
 echo "Running server"
 export SFM_RUN_SCHEDULER=True
 # source /etc/apache2/envvars
